49e12300885429f7150e64715aeda6bd8edadd48
Jessica <jessicagabriellearambula@gmail.com>
2022-03-31 18:25:20 -0600

    nuevo widgets

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,63 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 50,
+              width: 80,
+              color: Colors.blue,
+              child: const Center( child: Text(
+                'Columna',
+                style: TextStyle(color: Colors.black, fontSize: 20.0),
+              ),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                height: 70,
+                width: 830,
+                color: Colors.yellow,
+                  child: const Center( child: Text(
+                    'ROW',
+                    style: TextStyle(color: Colors.black, fontSize: 20.0),
+                  ),
+                  ),
+              ),
+              ],
+            ),
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                  child: const Center( child: Text(
+                    'stack',
+                    style: TextStyle(color: Colors.black, fontSize: 20.0),
+                  ),
+                  ),
+                ),
+                Container(
+                  width: 30,
+                  height: 30,
+                  color: Colors.white,
+                  child: const FlutterLogo(),
+                ),
+              ],
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.lightGreen,
+              child: const Center( child: Text(
+                'Contenedor',
+                style: TextStyle(color: Colors.black, fontSize: 20.0),
+              ),
+              ),
+            ),
+          ],
         ),
       ),
     );
